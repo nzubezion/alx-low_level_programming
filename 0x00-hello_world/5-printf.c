724e4302d42a9c676f243e9caf61cf1a6658a788
nzubezion <chukwunelozion@gmail.com>
2023-02-17 14:52:56 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - This prints out a string to stdout
- * Return: 0 (if successful)
+ * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
+ * Return: always 0.
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
